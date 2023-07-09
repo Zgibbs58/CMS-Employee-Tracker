60ce887ba8eac65aec51b3a9ac7aa44609095ff9
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-08 21:56:06 -0500

    Added the employee table and all relationships between data columns needed for these tables.

--- db/schema.sql
+++ db/schema.sql
@@ -17,5 +17,19 @@ CREATE TABLE
         title VARCHAR(30) NOT NULL,
         salary DECIMAL(10, 2) NOT NULL,
         department_id INT NOT NULL,
-        PRIMARY KEY(id)
-    )
+        PRIMARY KEY(id),
+        FOREIGN KEY (department_id) REFERENCES department(id)
+    );
+
+CREATE TABLE
+    employee (
+        id INT NOT NULL AUTO_INCREMENT,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT NOT NULL,
+        manager_id INT,
+        PRIMARY KEY(id),
+        FOREIGN KEY (role_id) REFERENCES role(id),
+        FOREIGN KEY (id) REFERENCES (manager_id) ON DELETE
+        SET NULL
+    );
