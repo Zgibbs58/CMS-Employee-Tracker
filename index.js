3935c5325b9489519633d563e0ab1f2b35254db7
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-15 18:24:39 -0500

    Made a lib folder and put the queries.js in there to hold all the queries for the program.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const inquirer = require("inquirer");
 const prompt = inquirer.createPromptModule();
-require("console.table");
-const db = require("./db/connection");
+const queries = require("./lib/queries");
 
-function initialPrompt() {
+function init() {
   prompt({
     type: "list",
     name: "query",
@@ -12,16 +11,16 @@ function initialPrompt() {
   }).then((answer) => {
     switch (answer.query) {
       case "View all employees":
-        viewEmployees();
+        queries.viewEmployees().then(init);
         break;
 
       case "View all roles":
-        viewRoles();
+        queries.viewRoles().then(init);
         break;
 
       case "View all departments":
         // handleView is a function that takes in a table name, queries the database for that table, and logs the results to the console.
-        handleView("departments");
+        queries.handleView("departments").then(init);
         break;
 
       case "Add an employee":
@@ -40,15 +39,17 @@ function initialPrompt() {
             type: "list",
             name: "role_id",
             message: "What is the employee's role?",
-            choices: selectRoles,
+            choices: queries.selectRoles,
           },
           {
             type: "list",
             name: "manager_id",
             message: "Who is the employee's manager?",
-            choices: selectManagers,
+            choices: queries.selectManagers,
           },
-        ]).then(addEmployee);
+        ])
+          .then(queries.addEmployee)
+          .then(init);
         break;
       case "Add a department":
         prompt([
@@ -57,94 +58,14 @@ function initialPrompt() {
             name: "name",
             message: "What is the name of the department you would like to add?",
           },
-        ]).then(addDepartment);
+        ])
+          .then(queries.addDepartment)
+          .then(init);
         break;
     }
   });
 }
 
-function handleView(table) {
-  db.query("SELECT * FROM ??", table, (err, results) => {
-    if (err) {
-      return console.error(err);
-    }
-    console.table(results);
-    initialPrompt();
-  });
-}
+init();
 
-function viewRoles() {
-  db.query(
-    "SELECT roles.id, roles.title, departments.name AS department, roles.salary FROM roles JOIN departments ON roles.department_id = departments.id",
-    (err, results) => {
-      if (err) {
-        return console.error(err);
-      }
-      console.table(results);
-      initialPrompt();
-    }
-  );
-}
-
-function viewEmployees() {
-  db.query(
-    "SELECT employees.id, employees.first_name, employees.last_name, roles.title AS title, departments.name AS department, roles.salary, CONCAT(managers.first_name, ' ', managers.last_name) AS manager FROM employees JOIN roles ON roles.id = employees.role_id JOIN departments ON roles.department_id = departments.id LEFT JOIN employees AS managers ON employees.manager_id = managers.id",
-    (err, results) => {
-      if (err) {
-        return console.error(err);
-      }
-      console.table(results);
-      initialPrompt();
-    }
-  );
-}
-
-function selectRoles() {
-  return db
-    .promise()
-    .query("SELECT * FROM roles")
-    .then(([data]) => {
-      return data.map(({ title, id }) => ({
-        name: title,
-        value: id,
-      }));
-    })
-    .catch((err) => console.error(err));
-}
-
-function selectManagers() {
-  return db
-    .promise()
-    .query("SELECT * FROM employees")
-    .then(([data]) => {
-      return data.map(({ first_name, last_name, id }) => ({
-        name: `${first_name} ${last_name}`,
-        value: id,
-      }));
-    })
-    .catch((err) => console.error(err));
-}
-
-function addEmployee(answer) {
-  db.query("INSERT INTO employees SET ?", answer, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Employee added successfully!");
-      initialPrompt();
-    }
-  });
-}
-
-function addDepartment(answer) {
-  db.query("INSERT INTO departments SET ?", answer, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Department added successfully!");
-      initialPrompt();
-    }
-  });
-}
-
-initialPrompt();
+module.exports = init;
